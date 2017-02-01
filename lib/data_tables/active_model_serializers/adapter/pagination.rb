ac42eaa38bc8fb67c8f3db89ececb50f9def00ad
Dale Stevens <dale@getnotion.com>
2017-02-01 16:00:21 -0700

    Restructure/DRY-up pagination. Do own limit/offset (no reliance on third-party gems)

--- lib/data_tables/active_model_serializers/adapter/pagination.rb
+++ lib/data_tables/active_model_serializers/adapter/pagination.rb
@@ -11,9 +11,38 @@ module DataTables
 
         def as_h
           {
-            recordsTotal: collection&.total_entries&.to_i,
-            recordsFiltered: collection&.unscope(:limit, :offset)&.count_estimate&.to_i
+            recordsTotal: records_total,
+            recordsFiltered: records_filtered
           }
+        end
+
+        protected
+
+        def records_filtered
+          @records_filtered ||= get_records_filtered
+        end
+
+        def records_total
+          @records_total ||= get_records_total
+        end
+
+        private
+
+        def get_records_filtered
+          if records_total < 1_000_000
+            @collection.unscope(:limit, :offset).count(@collection.model.primary_key).to_i
+          else
+            @collection.unscope(:limit, :offset).count_estimate.to_i
+          end
+        end
+
+        def get_records_total
+          count = @collection.model.all.count_estimate.to_i
+          if count < 1_000_000
+            count = @collection.model.all.count(@collection.model.primary_key).to_i
+          end
+
+          count
         end
 
       end
--- lib/data_tables/modules/pagination.rb
+++ lib/data_tables/modules/pagination.rb
@@ -3,6 +3,7 @@ require 'count_estimate'
 module DataTables
   module Modules
     class Pagination
+
       FIRST_PAGE = 1
 
       attr_reader :original_scope, :filtered_scope, :context
@@ -16,36 +17,9 @@ module DataTables
       def paginate
         start = (@request_parameters[:start] || '0').to_i
         length = (@request_parameters[:length] || '10').to_i
-        page = (start / length) + 1
-        @filtered_scope = @filtered_scope.paginate(page: page, per_page: length, total_entries: records_total)
+        page = (start / length)
+        @filtered_scope = @filtered_scope.offset(page * length).limit(length)
       end
-
-      def as_json
-        {
-          recordsTotal: @filtered_scope&.total_entries&.to_i,
-          recordsFiltered: records_filtered&.to_i
-        }
-      end
-
-      protected
-
-      def records_total
-        # TODO: Check threshold
-        count_estimate = @original_scope&.model&.all&.count_estimate.to_i
-        if count_estimate < 1_000_000
-          count_estimate = @original_scope&.count
-        end
-
-        count_estimate
-      end
-
-      def records_filtered
-        @filtered_scope&.unscope(:limit, :offset)&.count_estimate
-      end
-
-      attr_reader :adapter_options
-
-      private
 
     end
   end
