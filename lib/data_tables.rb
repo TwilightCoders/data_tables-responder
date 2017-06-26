ac6d8301b10df21792b56abb9430f3f36794acdf
Dale Stevens <dale@getnotion.com>
2017-06-26 16:23:01 -0600

    Clean up root path expansion utility

--- lib/data_tables.rb
+++ lib/data_tables.rb
@@ -1,6 +1,6 @@
 
 module DataTables
   def self.root
-    @root ||= Pathname.new(File.dirname(File.expand_path(File.dirname(__FILE__), '/../')))
+    @root ||= Pathname.new(File.expand_path('../', File.dirname(__FILE__)))
   end
 end
